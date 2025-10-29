2ff163cf1df0ee390d90967713d1310573ea9c93
Kaoutar laajil <129660727+kooutar@users.noreply.github.com>
2025-10-29 11:39:05 +0100

    feat: ajout de entity Commande ,controller

--- testSpring/src/main/java/com/kaoutar/testSpring/model/Commande.java
+++ testSpring/src/main/java/com/kaoutar/testSpring/model/Commande.java
@@ -1,4 +1,24 @@
 package com.kaoutar.testSpring.model;
 
+import com.kaoutar.testSpring.enums.StatusCommande;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+@Entity
 public class Commande {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int quantite;
+    private LocalDate dateCommande;
+    private StatusCommande status =StatusCommande.ENTREE;
+
+    @ManyToOne
+    @JoinColumn(name = "produit_id")
+    private Produit produit;
+
+    @ManyToOne
+    @JoinColumn(name = "fournisseur_id")
+    private Fournisseur fournisseur;
 }

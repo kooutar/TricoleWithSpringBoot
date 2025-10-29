8e6aa092545fd4be604e10b79400ed5a473f94dc
Kaoutar laajil <129660727+kooutar@users.noreply.github.com>
2025-10-29 11:29:18 +0100

    feat: implémentation du module Produit (controller, service, DTO, mapper, repository)

--- testSpring/src/main/java/com/kaoutar/testSpring/dto/ProduitDTO.java
+++ testSpring/src/main/java/com/kaoutar/testSpring/dto/ProduitDTO.java
@@ -1,4 +1,23 @@
 package com.kaoutar.testSpring.dto;
 
+import com.kaoutar.testSpring.model.Commande;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class ProduitDTO {
+    private Long id;
+    private String nom;
+    private Double prix_unitaire;
+    private String categorie;
+    private String  description;
+    private List<Commande> commandes;
+
+
 }

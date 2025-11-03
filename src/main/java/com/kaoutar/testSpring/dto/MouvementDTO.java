63badd265a3b4c5569e77493abe658cc58f8aade
Kaoutar laajil <129660727+kooutar@users.noreply.github.com>
2025-11-03 10:08:07 +0100

    maj MouvementDTO

--- src/main/java/com/kaoutar/testSpring/dto/MouvementDTO.java
+++ src/main/java/com/kaoutar/testSpring/dto/MouvementDTO.java
@@ -1,26 +1,22 @@
 package com.kaoutar.testSpring.dto;
-
+import com.kaoutar.testSpring.enums.StatutMouvement;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDate;
-@AllArgsConstructor
-@NoArgsConstructor
+import java.util.Date;
+
 @Getter
 @Setter
-
+@NoArgsConstructor
+@AllArgsConstructor
 public class MouvementDTO {
     private Long id;
-    private String statut;
+    private StatutMouvement statut;
     private Integer quantite;
-    private LocalDate dateMouvement;
-
-
+    private Date dateMouvement;
     private Long commandeId;
     private Long produitId;
     private String produitNom;
-
-
 }

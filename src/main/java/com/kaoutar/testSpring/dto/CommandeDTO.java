bb143b8b83c3308dd959c68acac91f431be49f62
Kaoutar laajil <129660727+kooutar@users.noreply.github.com>
2025-11-03 10:08:06 +0100

    maj CommandeDTO

--- src/main/java/com/kaoutar/testSpring/dto/CommandeDTO.java
+++ src/main/java/com/kaoutar/testSpring/dto/CommandeDTO.java
@@ -1,5 +1,6 @@
 package com.kaoutar.testSpring.dto;
 
+import com.kaoutar.testSpring.enums.StatusCommande;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.List;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -14,15 +16,11 @@ import java.util.List;
 public class CommandeDTO {
     private Long id;
     private LocalDate dateCommande;
-    private String statut;
-    private Double montantTotal;
+    private StatusCommande statut;
+    private Double totalCommande;
     private Integer quntite;
-
-
     private Long fournisseurId;
+    private String fournisseurNom; // Ajouter ce champ
+    private List<MouvementDTO> mouvements; // Ajouter ce champ
 
-
-    private String fournisseurNom;
-
-    private List<MouvementDTO> mouvements;
 }

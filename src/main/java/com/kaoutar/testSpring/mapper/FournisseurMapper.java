d67c7cc3bcfa289aa518298895709d32718e34c7
Kaoutar laajil <129660727+kooutar@users.noreply.github.com>
2025-11-03 10:08:09 +0100

    maj FournisseurMapper

--- src/main/java/com/kaoutar/testSpring/mapper/FournisseurMapper.java
+++ src/main/java/com/kaoutar/testSpring/mapper/FournisseurMapper.java
@@ -3,10 +3,15 @@ package com.kaoutar.testSpring.mapper;
 import com.kaoutar.testSpring.dto.FournisseurDTO;
 import com.kaoutar.testSpring.model.Fournisseur;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface FournisseurMapper {
     FournisseurDTO toDto(Fournisseur entity);
     Fournisseur toEntity(FournisseurDTO dto);
+    @Mapping(target = "id", ignore = true)
+    // ✅ Cette méthode met à jour un entity existant à partir d’un DTO
+    void updateFournisseurFromDto(FournisseurDTO dto, @MappingTarget Fournisseur fournisseur);
 }

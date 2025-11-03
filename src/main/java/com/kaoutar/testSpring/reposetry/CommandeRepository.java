d33f395d3d6eee31180625e0d8310119369dbe79
Kaoutar laajil <129660727+kooutar@users.noreply.github.com>
2025-11-03 10:08:19 +0100

    maj CommandeRepository

--- src/main/java/com/kaoutar/testSpring/reposetry/CommandeRepository.java
+++ src/main/java/com/kaoutar/testSpring/reposetry/CommandeRepository.java
@@ -1,4 +1,9 @@
 package com.kaoutar.testSpring.reposetry;
 
-public interface CommandeRepository {
+import com.kaoutar.testSpring.model.Commande;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommandeRepository extends JpaRepository<Commande, Long> {
 }

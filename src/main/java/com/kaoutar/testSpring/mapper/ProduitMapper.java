34f86bf1ff9437be875cc9f3cd8b2f5c84c4ed20
Kaoutar laajil <129660727+kooutar@users.noreply.github.com>
2025-11-03 10:08:10 +0100

    maj ProduitMapper

--- src/main/java/com/kaoutar/testSpring/mapper/ProduitMapper.java
+++ src/main/java/com/kaoutar/testSpring/mapper/ProduitMapper.java
@@ -1,12 +1,24 @@
 package com.kaoutar.testSpring.mapper;
 
 import com.kaoutar.testSpring.dto.ProduitDTO;
-
 import com.kaoutar.testSpring.model.Produit;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
-@Mapper(componentModel = "Spring")
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface ProduitMapper {
+
     ProduitDTO toDto(Produit entity);
+
+    List<ProduitDTO> toDtoList(List<Produit> entities);
+
     Produit toEntity(ProduitDTO dto);
+
+    @Mapping(target = "id", ignore = true)
+    void UpdateProduitFromDTO(ProduitDTO dto, @MappingTarget Produit entity);
+
+
 }

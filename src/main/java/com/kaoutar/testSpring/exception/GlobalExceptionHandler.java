bd1e7d15b02219bef2573d21667a93e3fb4e8d4e
Kaoutar laajil <129660727+kooutar@users.noreply.github.com>
2025-11-12 15:13:13 +0100

    feat: ajout handel des exeptions

--- src/main/java/com/kaoutar/testSpring/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kaoutar/testSpring/exception/GlobalExceptionHandler.java
@@ -1,12 +1,15 @@
 package com.kaoutar.testSpring.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.swing.text.html.parser.Entity;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
@@ -22,7 +25,33 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
     @ExceptionHandler(Exception.class)
-    public  ResponseEntity<String> handleGeneralError(Exception ex){
-        return new ResponseEntity<>("An error occurred: " + ex.getMessage(), org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
+    public  ResponseEntity<Map<String, String>> handleGeneralError(Exception ex){
+        Map error = new HashMap();
+        error.put("error",ex.getMessage());
+        return new ResponseEntity<>(error, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        return new  ResponseEntity<>(errors,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(org.springframework.dao.DataIntegrityViolationException.class)
+    public ResponseEntity<Map<String, String>> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        Map<String, String> error = new HashMap<>();
+
+        if (ex.getMessage().contains("fournisseur.email")) {
+            error.put("email", "Cet email existe déjà !");
+        } else {
+            error.put("error", "Erreur de base de données : contrainte violée.");
+        }
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
     }
 }

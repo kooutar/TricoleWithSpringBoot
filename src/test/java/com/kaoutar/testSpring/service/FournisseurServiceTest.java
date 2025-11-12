b72c78330a8b40b1df595a25aa9b2a6d14250dd0
Kaoutar laajil <129660727+kooutar@users.noreply.github.com>
2025-11-12 15:21:42 +0100

    refactor: transformer les test de testd'integration vers test unitaire

--- src/test/java/com/kaoutar/testSpring/service/FournisseurServiceTest.java
+++ src/test/java/com/kaoutar/testSpring/service/FournisseurServiceTest.java
@@ -1,4 +1,115 @@
-package com.kaoutar.testSpring.controller;
+package com.kaoutar.testSpring.service;
 
-public class FournisseurServiceTest {
+import com.kaoutar.testSpring.TestSpringApplication;
+import com.kaoutar.testSpring.dto.FournisseurDTO;
+import com.kaoutar.testSpring.mapper.FournisseurMapper;
+import com.kaoutar.testSpring.model.Fournisseur;
+import com.kaoutar.testSpring.reposetry.FournisseurRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class FournisseurServiceTest {
+
+    @Mock
+    private FournisseurRepository repo;
+
+    @Mock
+    private FournisseurMapper mapper;
+
+    @InjectMocks
+    private FournisseurService fournisseurService;
+
+    @Test
+    void testSaveFournisseur() {
+
+        FournisseurDTO dto = new FournisseurDTO();
+        dto.setSociete("Société X");
+        dto.setEmail("emailX@gmail.com");
+
+        Fournisseur entity = new Fournisseur();
+        entity.setSociete("Société X");
+        entity.setEmail("emailX@gmail.com");
+
+       when(mapper.toEntity(dto)).thenReturn(entity);
+       when(repo.save(entity)).thenReturn(entity);
+       when(mapper.toDto(entity)).thenReturn(dto);
+
+
+        FournisseurDTO saved = fournisseurService.save(dto);
+
+        assertNotNull(saved);
+        assertEquals("Société X",saved.getSociete());
+    }
+
+  @Test
+    void testGetFournisseurById(){
+        Fournisseur fournisseur = new Fournisseur();
+        fournisseur.setId(1L);
+        fournisseur.setSociete("TestSociete");
+
+        FournisseurDTO dto = new FournisseurDTO();
+        dto.setId(1L);
+        dto.setSociete("TestSociete");
+
+        when(repo.findById(1L)).thenReturn(Optional.of(fournisseur));
+        when(mapper.toDto(fournisseur)).thenReturn(dto);
+
+        FournisseurDTO result= fournisseurService.getFournisseurById(1L);
+        assertEquals(1L,result.getId());
+        assertEquals("TestSociete",result.getSociete());
+      verify(repo,times(1)).findById(1L);
+  }
+
+  @Test
+    void  testDeleteFournisseur(){
+        Fournisseur fournisseur = new Fournisseur();
+        fournisseur.setId(1L);
+
+        when(repo.findById(1L)).thenReturn(Optional.of(fournisseur));
+        String message = fournisseurService.deleteFournisseur(1L);
+        assertEquals("delete with success", message);
+        verify(repo).delete(fournisseur);
+  }
+  @Test
+    void testUpdateFournisseur(){
+        FournisseurDTO dto = new FournisseurDTO();
+          dto.setSociete("Société Modifiée");
+          dto.setAdresse("Nouvelle Adresse");
+          dto.setContact("Nouveau Contact");
+          dto.setEmail("modifie@email.com");
+          dto.setTelephone("0123456789");
+          dto.setVille("Rabat");
+          dto.setIce("1234567890");
+         Fournisseur existingEntity = new Fournisseur();
+         existingEntity.setId(1L);
+         existingEntity.setSociete("Auncienne societe ");
+         when(mapper.toEntity(dto)).thenReturn(existingEntity);
+         when(mapper.toDto(existingEntity)).thenReturn(dto);
+         when(repo.findById(1L)).thenReturn(Optional.of(existingEntity));
+
+        FournisseurDTO updeted= fournisseurService.updateFournisseur(1L,dto);
+        assertNotNull(updeted);
+        assertEquals(dto.getSociete(),updeted.getSociete());
+
+
+
+
+  }
+
 }
